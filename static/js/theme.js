5961bf98a88e5e45e99871a2167c893390d09c0d
Sandeepa Induwara Samaranayake <95087710+SandeepaInduwaraSamaranayake@users.noreply.github.com>
2025-01-23 21:21:37 +0530

    theme effect added to the model preview window as well

--- static/js/theme.js
+++ static/js/theme.js
@@ -16,6 +16,8 @@ lightThemeIcon.addEventListener('click', () => {
     localStorage.setItem('theme', 'light-mode'); // Save user preference
     lightThemeIcon.style.display = 'none'; // Hide light icon
     darkThemeIcon.style.display = 'inline'; // Show dark icon
+
+    updateModalTheme('light-mode'); // Update modal theme
 });
 
 darkThemeIcon.addEventListener('click', () => {
@@ -24,4 +26,23 @@ darkThemeIcon.addEventListener('click', () => {
     localStorage.setItem('theme', 'dark-mode'); // Save user preference
     darkThemeIcon.style.display = 'none'; // Hide dark icon
     lightThemeIcon.style.display = 'inline'; // Show light icon
+
+    updateModalTheme('dark-mode'); // Update modal theme
 });
+
+
+// Function to update modal theme
+function updateModalTheme(theme) 
+{
+    const modalContent = document.querySelector('.modal-content');
+    if (theme === 'light-mode') 
+    {
+        modalContent.classList.remove('dark-mode');
+        modalContent.classList.add('light-mode');
+    } 
+    else 
+    {
+        modalContent.classList.remove('light-mode');
+        modalContent.classList.add('dark-mode');
+    }
+}
